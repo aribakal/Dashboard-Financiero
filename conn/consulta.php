c2431f543b29bdf2ab00fb8f6d6bf96a396bc1a2
Ari <bakalariel2002@gmaill.com>
2020-12-02 19:03:20 -0300

    Modificaciones de Uri

--- conn/consulta.php
+++ conn/consulta.php
@@ -1,9 +1,25 @@
 <?php
 
-$consulta = mysqli_query($conexion, "SELECT sum(cd.total) from tesoreria.cobranzas_distribucion cd
+/*$consulta = mysqli_query($conexion, "SELECT sum(cd.total) from tesoreria.cobranzas_distribucion cd
 left join tesoreria.cobranzas_cabe cc on cc.idcobranza = cd.idcobranza
 where cd.idagrupamiento = 1 and month(cc.fecha) = 9 and year(cc.fecha) = 2020");
+*/
 
-$result = mysqli_fetch_array($consulta);
+
+function getDato( $conexion , $idgrupo , $mes , $anio ) {
+
+  $consulta = mysqli_query($conexion, "SELECT sum(cd.total) as total , count(*) cantidad from tesoreria.cobranzas_distribucion cd
+  left join tesoreria.cobranzas_cabe cc on cc.idcobranza = cd.idcobranza
+  where cd.idagrupamiento = $idgrupo and month(cc.fecha) = $mes and year(cc.fecha) = $anio");
+
+  $result = mysqli_fetch_array($consulta);
+
+  return array( "total" => $result["total"] , "cantidad" => $result["cantidad"] ) ;
+
+}
+
+//array( "enero" => getDato( 1 , 1 , 2020 ) , "febrero" => getDato( 1 , 2 , 2020 ))
+
+//$result = mysqli_fetch_array($consulta);
 
 ?>
